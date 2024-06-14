dbe2cc8372f0e138e115591c68b880dfc314d2b9
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-06-13 21:47:34 -0400

    Added try and catch block of code

--- index.js
+++ index.js
@@ -2,10 +2,15 @@
 import { central, db1, db2, db3, vault } from "./databases.js";
 
 async function getUserData(id) {
-
+try{
   const dbs = {
     db1: db1,
     db2: db2,
     db3: db3
   };
+  //Access the central database that returns a string that identifies which database to access for that particular user's information.
+  const centralDB = await central(id);
+
+}catch(e){
+  console.log(e.message)}
 }
